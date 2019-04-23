669819cc6405071a05af63104a504c5aeebe6784
Bill Fienberg <billfienberg@gmail.com>
2019-04-23 05:17:02 -0500

    Finish tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
-import Header from "../components/Header";
+import Layout from "../components/MyLayout.js";
+import Link from "next/link";
 
-export default function Index() {
+function getPosts() {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
+  ];
+}
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
+export default function Blog() {
   return (
-    <div>
-      <Header />
-      <p>Hello Next.js</p>
-    </div>
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1 {
+          font-family: "Arial";
+        }
+      `}</style>
+    </Layout>
   );
 }
